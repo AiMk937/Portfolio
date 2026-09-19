8f073939262c422befd1a8768d4f1987dde084ae
Aimaan Khan <theanonymous.ak@gmail.com>
2026-09-18 20:03:15 -0700

    Update contact.js

--- data/contact.js
+++ data/contact.js
@@ -5,6 +5,10 @@ export const contact = {
   links: [
     { label: "Email", handle: "aimaanjkhaan@gmail.com", href: "mailto:aimaanjkhaan@gmail.com", icon: "fa-solid fa-envelope" },
     { label: "LinkedIn", handle: "linkedin.com/in/aimaan-khan", href: "https://www.linkedin.com/in/aimaan-khan", icon: "fa-brands fa-linkedin" },
-    { label: "GitHub", handle: "github.com/AiMk937", href: "https://github.com/AiMk937", icon: "fa-brands fa-github" }
+    { label: "GitHub", handle: "github.com/AiMk937", href: "https://github.com/AiMk937", icon: "fa-brands fa-github" },
+    { label: "Kaggle", handle: "kaggle.com/aimaankhan", href: "https://www.kaggle.com/aimaankhan", icon: "fa-brands fa-kaggle" },
+    { label: "LeetCode", handle: "leetcode.com/u/AiM937", href: "https://leetcode.com/u/AiM937/", icon: "fa-solid fa-code" },
+    // { label: "HackerRank", handle: "hackerrank.com/aimaanjkhaan", href: "https://www.hackerrank.com/profile/aimaanjkhaan", icon: "fa-brands fa-hackerrank" },
+    { label: "Credly", handle: "credly.com/users/aimaan-khan", href: "https://www.credly.com/users/aimaan-khan.d4248b5d", icon: "fa-solid fa-certificate" }
   ]
 };
